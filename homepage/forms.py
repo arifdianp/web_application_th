a90977ee6222a7ab9729a60d5d67bd241d050314
Arif Dianprakasa <dparief32@gmail.com>
2022-12-18 10:55:09 +0700

    make some migration to model changes

--- homepage/forms.py
+++ homepage/forms.py
@@ -1,7 +1,23 @@
 from django import forms
 from .models import Post
 
+PROGRESS_CHOICES = ("Not Mitigated", "Partially Mitigated", "Fully Mitigated")
+
+
 class create_form(forms.ModelForm):
+    class Meta:
+        model = Post
+        #fields = ('name','description','mitigation_strategy')
+        fields = '__all__'
+        widgets = {
+            'name': forms.TextInput(attrs={'class':'form-control col-sm-5'}),
+            'description': forms.Textarea(attrs={'class':'form-control'}),
+            'mitigation_strategy': forms.Textarea(attrs={'class':'form-control'}),
+            'mitigation_progress': forms.Select(attrs={'class':'form-control col-sm-2'}),
+            'last_updated_by': forms.TextInput(attrs={'class':'form-control','value':'a','id':'who_updates', 'type':'hidden'})
+        }
+
+class edit_form(forms.ModelForm):
     class Meta:
         model = Post
         #fields = ('name','description','mitigation_strategy')
@@ -10,16 +26,6 @@ class create_form(forms.ModelForm):
             'name': forms.TextInput(attrs={'class':'form-control'}),
             'description': forms.Textarea(attrs={'class':'form-control'}),
             'mitigation_strategy': forms.Textarea(attrs={'class':'form-control'}),
-            'last_updated_by': forms.Select(attrs={'class':'form-control'})
-        }
-
-class edit_form(forms.ModelForm):
-    class Meta:
-        model = Post
-        #fields = ('name','description','mitigation_strategy')
-        fields = ('name','description','mitigation_strategy')
-        widgets = {
-            'name': forms.TextInput(attrs={'class':'form-control'}),
-            'description': forms.Textarea(attrs={'class':'form-control'}),
-            'mitigation_strategy': forms.Textarea(attrs={'class':'form-control'}),
+            #'mitigation_progress': forms.ChoiceField(choices=PROGRESS_CHOICES),
+            'last_updated_by': forms.TextInput(attrs={'class':'form-control','value':'a','id':'who_updates', 'type':'hidden'})
         }
--- homepage/models.py
+++ homepage/models.py
@@ -2,15 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+PROGRESS_CHOICES = [
+    ("Not Mitigated", "Not Mitigated"),
+    ("Partially Mitigated", "Partially Mitigated"),
+    ("Fully Mitigated", "Fully Mitigated")
+]
+
 # Create your models here.
 class Post(models.Model):
     name = models.CharField(max_length=300)
     description = models.TextField()
     mitigation_strategy = models.TextField()
+    mitigation_progress = models.CharField(max_length=200, choices=PROGRESS_CHOICES, default="Not Mitigated")
     last_updated_by = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name 
+        return self.name
 
     def get_absolute_url(self):
         return reverse('home')
